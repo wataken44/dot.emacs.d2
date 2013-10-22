c791e8d6f170f8db633ebf16af1f2ad15ac5ebb5
Kenichi Watanabe <wataken44@maria.local>
2013-10-22 23:15:06 +0900

    fix: use let* instead of setq to remove compile warnings

--- my-lisp/switch-to-user-buffer.el
+++ my-lisp/switch-to-user-buffer.el
@@ -11,30 +11,33 @@
 ; switch to next user's buffer
 ; name of user's buffer start with [^ *] 
 (defun switch-to-user-buffer (switch-dir)
-  ;; sort buffer-list
-  (setq sorted-buffer-alist (sort (buffer-list) 'buffer-name-less))
-  ;; get current buffer index in sorted buffer-list
-  (setq buffer-list-length (length sorted-buffer-alist)) 
-  (setq current-buffer-index 
-        (- buffer-list-length
-           (length (member (current-buffer) sorted-buffer-alist))))
-  ;; search and jump to next user buffer
-  ;; start from current-buffer-index + switch-dir and wrap around
-  (dotimes (i buffer-list-length)
-    (setq buffer-index
-          (% (+ current-buffer-index
-                buffer-list-length
-                (* (+ 1 i) switch-dir)) 
-             buffer-list-length))
-    (setq dest-buffer (nth buffer-index sorted-buffer-alist))
-    (setq dest-buffer-name (buffer-name dest-buffer))
-    (if (or (string-match-p "^[^ *]" dest-buffer-name)
-            (member dest-buffer-name stub-special-buffer-alist))
-        (progn (switch-to-buffer dest-buffer)
-               (return t))
-      )
-    )
-  )
+  (let* (
+        ;; sort buffer-list
+        (sorted-buffer-alist (sort (buffer-list) 'buffer-name-less))
+        ;; get current buffer index in sorted buffer-list
+        (buffer-list-length (length sorted-buffer-alist)) 
+        (current-buffer-index 
+         (- buffer-list-length
+            (length (member (current-buffer) sorted-buffer-alist))))
+        )
+    ;; search and jump to next user buffer
+    ;; start from current-buffer-index + switch-dir and wrap around
+    (dotimes (i buffer-list-length)
+      (let* (
+            (buffer-index
+             (% (+ current-buffer-index
+                   buffer-list-length
+                   (* (+ 1 i) switch-dir)) 
+                buffer-list-length))
+            (dest-buffer (nth buffer-index sorted-buffer-alist))
+            (dest-buffer-name (buffer-name dest-buffer))
+            )
+        (if (or (string-match-p "^[^ *]" dest-buffer-name)
+                (member dest-buffer-name stub-special-buffer-alist))
+            (progn (switch-to-buffer dest-buffer)
+                   (return t)))
+        )
+      )))
 
 ;; switch forward
 (defun switch-to-user-buffer-next ()

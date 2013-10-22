3bcfa88bd60e54016cf4b5f1bec6192399077dc8
Kenichi Watanabe <wataken44@maria.local>
2013-10-23 00:53:20 +0900

    add: font setting, key bind

--- init.d/init-linux-x-window.el
+++ init.d/init-linux-x-window.el
@@ -1,10 +1,14 @@
 
+;; set font
+(set-frame-font "-*-Takaoゴシック-*-*-*-*-18-*-*-*-*-*-*-*")
+(setq x-select-enable-clipboard t)
+
 (setq quail-japanese-use-double-n t)
 
 (require 'mozc)
 (setq default-input-method "japanese-mozc")
 (setq mozc-candidate-style 'echo-area)
 
-(global-set-key (kbd "<C-slash>") 'toggle-input-method)
+(global-set-key (kbd "C-/") 'toggle-input-method)
 
 (provide 'init-linux-x-window)

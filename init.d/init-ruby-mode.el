e4d7678cd9942c330240224f300c3674092f8e5b
U-tembin\wataken44 <wataken44@tembin.(none)>
2014-05-10 08:35:39 +0900

    [F] fix indent style

--- init.d/init-ruby-mode.el
+++ init.d/init-ruby-mode.el
@@ -3,6 +3,10 @@
           '(lambda ()
              (setq indent-tabs-mode nil)
              (setq ruby-indent-level 4)
+             (setq ruby-deep-indent-paren nil)
+             (setq ruby-deep-indent-paren-style nil)
+             (setq ruby-deep-arglist nil)
+             (define-key ruby-mode-map [return] 'ruby-reindent-then-newline-and-indent)
              ))
 
 (provide 'init-ruby-mode)

3bc8f761221536e7300a374b8b1118ad859fd622
U-tembin\wataken44 <wataken44@localhost.localdomain>
2014-10-13 11:29:23 +0900

    [F] change to melpa-stable from marmalade

--- init.d/init-elpa.el
+++ init.d/init-elpa.el
@@ -1,6 +1,6 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)

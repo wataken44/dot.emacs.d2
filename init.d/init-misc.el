0dd327169ee1d858e1c723b4a76a9451b238e44e
wataken44 <wataken44@gmail.com>
2017-01-12 12:15:24 +0900

    [A] markdown mode; [U] update packages

--- init.d/init-misc.el
+++ init.d/init-misc.el
@@ -29,6 +29,9 @@
 ; region mark visible
 (setq-default transient-mark-mode t)
 
+; tab width 
+(setq-default tab-width 4)
+
 ; do not use tab by default
 (setq-default indent-tabs-mode nil)
 
--- init.el
+++ init.el
@@ -48,3 +48,18 @@
 (require 'init-octave-mode)
 (require 'init-uniquify)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-ff-auto-update-initial-value nil)
+ '(package-selected-packages
+   (quote
+    (markdown-mode smartparens helm google-c-style go-snippets go-mode go-autocomplete deferred))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-source-header ((t (:background "#22083397778B" :foreground "white")))))

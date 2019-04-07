6796299ee83751bedd78632716d43457d90605c3
wataken44 <wataken44@gmail.com>
2019-04-07 23:55:16 +0900

    update packages

--- elpa/google-c-style-20180130.936/google-c-style-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "google-c-style" "20180130.936" "Google's C/C++ style for c-mode" 'nil :commit "f9347e1e9d79aee9cde0802fe178d72c8f87926c" :keywords '("c" "tools"))

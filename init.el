06369bc2abebce31abafdf712ed9ca868b44132b
watanabek330 <kenichi.watanabe@g.softbank.co.jp>
2019-10-10 15:27:01 +0900

    update packages

--- init.el
+++ init.el
@@ -57,7 +57,7 @@
  '(helm-ff-auto-update-initial-value nil)
  '(package-selected-packages
    (quote
-    (blacken typescript-mode markdown-mode smartparens helm google-c-style go-snippets go-mode go-autocomplete deferred))))
+    (blacken google-c-style go-snippets go-autocomplete deferred))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

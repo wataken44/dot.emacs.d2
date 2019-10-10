e621050ad0648e59015274e53880ea58b5c65aa8
watanabek330 <kenichi.watanabe@g.softbank.co.jp>
2019-10-10 15:35:35 +0900

    add blacken setup

--- init.d/init-python-mode.el
+++ init.d/init-python-mode.el
@@ -5,4 +5,12 @@
              (setq indent-tabs-mode nil)
              ))
 
+(if (= 0 (shell-command "which black 2>&1 > /dev/null"))
+    (progn 
+      (require 'blacken)
+      (add-hook 'python-mode-hook 'blacken-mode)
+      )
+  )
+
+
 (provide 'init-python-mode)

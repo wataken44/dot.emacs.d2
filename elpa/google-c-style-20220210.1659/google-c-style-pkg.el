c6d56e8a9e922888f8d5d155d3ffd5ef7e812ca1
wataken44 <wataken44@gmail.com>
2022-04-27 23:22:06 +0900

    add packages

---
+++ elpa/google-c-style-20220210.1659/google-c-style-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from google-c-style.el  -*- no-byte-compile: t -*-
+(define-package "google-c-style" "20220210.1659" "Google's C/C++ style for c-mode" 'nil :commit "2eb7e16647b16cf6879d47c9b96cedb6b3d3983a" :keywords '("c" "tools"))
--- elpa/helm-core-20200829.1025/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20200829.1025" "Development files for Helm"
-  '((emacs "25.1")
-    (async "1.9.4"))
-  :commit "0fa80a8b62eba89e0510cac5a5f8009573ce6933" :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:

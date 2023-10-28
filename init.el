38c6645b123a9410bfe05a3f9ee94d0627e977f0
wataken44 <wataken44@hotmail.com>
2023-10-28 11:25:42 +0900

    update packages

--- init.el
+++ init.el
@@ -42,3 +42,16 @@
 (require 'init-octave-mode)
 (require 'init-typescript-mode)
 (require 'init-uniquify)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(typescript-mode smartparens markdown-mode helm google-c-style go-snippets go-mode go-autocomplete deferred blacken)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
